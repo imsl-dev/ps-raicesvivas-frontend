210ae0148439ef869d665d03b79ad865fe2c48c8
juanbauza00 <juanpablobauza01@gmail.com>
2025-10-09 19:29:27 -0300

    Added events components

---
+++ src/app/components/pages/events/nuevo-evento/nuevo-evento.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-evento',
+  imports: [],
+  templateUrl: './nuevo-evento.html',
+  styleUrl: './nuevo-evento.css'
+})
+export class NuevoEvento {
+
+}
